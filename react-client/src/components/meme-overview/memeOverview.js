e7b873b9aed4f4e9ae871717ced178ee9cf5c593
nesskopf <vanessa.voigt@campus.lmu.de>
2021-01-19 12:19:42 +0100

    load memes worsk

--- react-client/src/components/meme-overview/memeOverview.js
+++ react-client/src/components/meme-overview/memeOverview.js
@@ -10,25 +10,48 @@ class MemeOverview extends React.Component {
     };
   }
 
-
-  loadMemes = () => {
-    console.log('load Memes');
-    fetch('/generatedMemes/getMemes')
+    componentDidMount(){
+      console.log("Anwendung gestartet");
+        fetch('/generatedMemes/getMemes')
             .then(res => {
                 console.log(res);
                 return res.json()
-             })
+            })
             .then(memes => {
-                console.log(memes);
-             });
+                console.log(memes.memes[1]);
+                this.setState({
+                    memes: memes.memes,  // meme array is wrapped in meme json
+                })
+            });
+    }
+
+
+  loadMemes = () => {
+      const memes = this.state.memes
+    console.log('load Memes');
+      for (var i = 0; i < memes.length; i++) {
+          console.log(this.state.memes[i].url)
+      }
+
   }
 
 
   render() {
+
+      const items = [];
+      let memes = this.state.memes
+      for (var i = 0; i < memes.length; i++) {
+          console.log(this.state.memes[i].url)
+          let url = this.state.memes[i].url
+          items.push(<img src={url} />)
+
+      }
     return (
       <div >
         <button onClick={this.loadMemes}>Load</button>
-
+          <div>
+            {items}
+          </div>
       </div>
     )
   }
