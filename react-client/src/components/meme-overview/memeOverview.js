31387b47da9ce05af9254fd0c8feec0684d9a1e7
nesskopf <vanessa.voigt@campus.lmu.de>
2021-02-05 22:50:33 +0100

    started Single View

--- react-client/src/components/meme-overview/memeOverview.js
+++ react-client/src/components/meme-overview/memeOverview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IoIosThumbsUp, IoIosThumbsDown } from 'react-icons/io';
+import {IoIosThumbsUp, IoIosThumbsDown} from 'react-icons/io';
 
 import './memeOverview.css';
 import Meme from "../Meme/meme";
@@ -10,10 +10,10 @@ class OverviewElem extends React.Component {
         let upVotes = meme.upVotes
         let downVotes = meme.downVotes
 
-        if(isUpvote){
+        if (isUpvote) {
             console.log('upVote')
             upVotes = upVotes + 1
-        } else if(!isUpvote){
+        } else if (!isUpvote) {
             console.log('downVote')
             downVotes = downVotes + 1
         } else {
@@ -70,60 +70,38 @@ class OverviewElem extends React.Component {
                     topText={topCaption}
                     bottomText={bottomCaption}
                 />
-                <button onClick={() => this.vote(meme, true)}><IoIosThumbsUp /></button>
+                <button onClick={() => this.vote(meme, true)}><IoIosThumbsUp/></button>
                 <button onClick={() => this.vote(meme, false)}><IoIosThumbsDown/></button>
-                <p><IoIosThumbsUp />: {upVotes}</p>
+                <p><IoIosThumbsUp/>: {upVotes}</p>
                 <p><IoIosThumbsDown/>: {downVotes}</p>
             </div>
         )
     }
 }
 
-class MemeOverview extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            memes: []
-        };
+class Grid extends React.Component{
+
+
+
+    getMemesFromDb = () => {
+        this.props.getMemes();
     }
-
-    componentDidMount() {
-
-        this.getMemesFromDb(this.state)
-    }
-
-    getMemesFromDb(state) {
-
-        fetch('/generatedMemes/getMemes')
-            .then(res => {
-                return res.json()
-            })
-            .then(memes => {
-
-                this.setState({
-                    memes: memes.memes,  // meme array is wrapped in meme json
-                })
-
-            });
-    }
-
 
     render() {
 
+
+
         const items = [];
-        let memes = this.state.memes
+        let memes = this.props.memes
         for (let i = 0; i < memes.length; i++) {
             let id = memes[i]._id
 
-            items.push(<OverviewElem className="gridItem"
-                                     key={id}
-                                     meme={memes[i]}
-                                     getMemes={() => {
-                                         this.getMemesFromDb()
-                                     }}
-                                     testPrint={() => {
-                                         this.testPrint()
-                                     }}
+            items.push(<OverviewElem
+                className="gridItem"
+                key={id}
+                meme={memes[i]}
+                getMemes={() => {this.getMemesFromDb()}}
+                testPrint={() => {this.testPrint()}}
             />)
 
 
@@ -139,6 +117,136 @@ class MemeOverview extends React.Component {
         )
     }
 }
+
+class SingleView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            i: 0
+        };
+    }
+
+    previous(){
+        let memes = this.props.memes
+        let i = this.state.i
+        if(i > 0){
+            i = i-1
+        } else {
+            i = memes.length-1
+        }
+
+
+        this.setState({
+            i: i
+        })
+
+
+    }
+
+    next(){
+        let memes = this.props.memes
+        let i = this.state.i
+        console.log('nächstes Bild')
+        if( i < memes.length-1){
+            i = i+1
+        } else {
+            i = 0
+        }
+
+
+        this.setState({
+            i: i
+        })
+    }
+
+
+
+    render(){
+
+
+
+        const items = [];
+        let memes = this.props.memes
+        for (let i = 0; i < memes.length; i++) {
+            let id = memes[i]._id
+
+            items.push(<OverviewElem
+                className="gridItem"
+                key={id}
+                meme={memes[i]}
+                getMemes={() => {this.getMemesFromDb()}}
+                testPrint={() => {this.testPrint()}}
+            />)
+
+
+        }
+
+
+
+
+
+
+        return(
+            <div>
+                <h1>Die coolste Single View aller Zeiten</h1>
+                <div>
+                    <button onClick={() => this.previous()}>previous</button>
+                    {items[this.state.i]}
+                    <button onClick={() => this.next()}>next</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+class MemeOverview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            memes: []
+        };
+    }
+
+    componentDidMount() {
+        this.getMemesFromDb()
+    }
+
+    getMemesFromDb() {
+
+        fetch('/generatedMemes/getMemes')
+            .then(res => {
+                return res.json()
+            })
+            .then(memes => {
+
+                this.setState({
+                    memes: memes.memes,  // meme array is wrapped in meme json
+                })
+
+            });
+    }
+
+    render(){
+        return(
+            <div>
+                <Grid
+                    memes={this.state.memes}
+                    getMemes={() => {this.getMemesFromDb()}}
+                />
+                <SingleView
+                    memes={this.state.memes}
+                    getMemes={() => {this.getMemesFromDb()}}
+                />
+            </div>
+
+        )
+    }
+
+
+
+}
+
+
 
 
 // ========================================
